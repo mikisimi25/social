73e5e4bd90e8b6359998fa612f3c2702bc0b508f
Maksym Ostapenko <mikisimi25.dev@gmail.com>
2023-01-08 15:18:40 +0100

    Added router structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 import './App.scss';
-import UserListItem from './components/UserListItem/UserListItem';
-import * as UserService from './services/User.service.jsx';
-
+import Users from './pages/Users/Users';
 const App = () => {
-  const [ userColl, setUserColl ] = useState([]);
-
-  useEffect(() => {
-    UserService.getUser()
-        .then(({data}) => {
-            setUserColl(data);
-        })
-  }, []);
 
   return (
     <div className="App container">
       <div className="row justify-content-center pt-md-3">
-        <div className="col-12 col-md-6">
-          <h2>LIST OF USERS</h2>
-          <ul>
-            {
-              setUserColl.length ?
-              <li className={'notFoundItem'}>USERS NOT FOUND</li>
-              :
-              userColl.map( user => <UserListItem data={user} /> )
-            }
-          </ul>
-        </div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={ <Users /> } />
+          </Routes>
+        </BrowserRouter>
       </div>
     </div>
   );
