29ed594e619b0ca35c80e1ebf261c3c7ac0b6d25
Maksym Ostapenko <mikisimi25.dev@gmail.com>
2023-03-13 23:45:09 +0100

    Basic menu

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,25 @@ import Login from './pages/Login/Login';
 
 const App = () => {
 
+  const menuItems = [
+    { name: 'Home', link: '/' },
+    { name: 'Login', link: '/login' },
+  ];
+
   return (    
     <div className="App container">
       <div className="row justify-content-center pt-md-3">
+
+        <nav>
+          <ul>
+            {menuItems.map((item) => (
+              <li key={item.name}>
+                <a href={item.link}>{item.name}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        
         <BrowserRouter>
           <Routes>
             <Route path="/" element={ <Users /> } />
